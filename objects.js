cd29567539d59925bce79b32c4dbc1845075e876
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-09 01:24:20 +0000

    Automatically backed up by Learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {
+  artist1: "song", 
+  artist2: "song"};
+  //the limitation imposed is that you can't have more than one song by the same artist
+  
+  function updatePlaylist (playlist, artist, song){
+    
+  }
+  
+ function removeFromPlaylist(playlisy, artist){
+   
+ }
