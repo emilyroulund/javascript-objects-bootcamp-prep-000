cb329ace98cd7a0b2211d48fd2869b3e828bd962
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-09 02:05:10 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -11,6 +11,6 @@ var playlist = {
   
  function removeFromPlaylist(playlist, artistName){
    delete playlist.artistName;
-   return playlist.assign({}, playlist); 
+   return Object.assign({}, playlist); 
  }
  //body of function should delete the key-value pair from the playlist and return the updated playlist 
