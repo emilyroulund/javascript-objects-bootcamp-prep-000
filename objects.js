a62d1afad9876b9e60fac4e6cd8b117d1e8eb81c
merry-lab-3791 <emilyroulund@gmail.com>
2019-03-09 01:53:06 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,7 +4,8 @@ var playlist = {
   //the limitation imposed is that you can't have more than one song by the same artist
   
   function updatePlaylist (playlist, artistName, songTitle){
-    return obj
+    playlist[artistName] = songTitle
+    return playlist
   }
   //accepts three paramenters; body should add the song and artist as a key-value pair to playlist object 
   
